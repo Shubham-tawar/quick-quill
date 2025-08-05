808dc33c0df42652ed43ce4b3dd78efe158ffb5d
Shubham <shubhamop164@gmail.com>
2025-08-05 11:08:41 +0530

    MonogDB Atlas Connected

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/quickquill"; 
+const mongoURI = process.env.MONGODB_URI;
+
+
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(mongoURI);
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("Connected to MongoDB successfully");
     } catch (error) {
         console.error("MongoDB connection error:", error);
